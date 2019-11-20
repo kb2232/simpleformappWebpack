776b64e61da8f879c251a1b551974ff9e8d3e833
kunle babatunde <kb2232@nyu.edu>
2019-11-20 05:09:05 -0500

    Added the search bar and onchange handle events

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+import App from './components/App';
 
-const App=()=>{
-  return(
-    <div className="component">
-      HELLO APP
-    </div>
-  )
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
